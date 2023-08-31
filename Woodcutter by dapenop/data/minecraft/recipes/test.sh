3bede8abdbeb7f89ef712764782f2dc94bbaeacb
dapenp <antontorchuk3@gmail.com>
2023-08-31 15:33:25 +0300

    clear trash

--- Woodcutter by dapenop/data/minecraft/recipes/test.sh
+++
@@ -1,5 +0,0 @@
-find . -type f -name '*bamboo_fence*' | while read FILE ; do
-    #newfile="$(echo ${FILE} |sed 's/crimson/warped/g')" ;
-    #mv "${FILE}" "${newfile}" ;
-    sed -i 's/4/2/g' "${FILE}" ;
-done
